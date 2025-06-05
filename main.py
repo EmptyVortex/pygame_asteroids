d56d9a5cef081a6be1717cf02d418d776c6960f6
EmptyVortex <malcolmshire12@gmail.com>
2025-06-05 11:38:58 +0100

    add window

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 # this allows us to use code from
 # the open-source pygame library
 # throughout this file
+
 import pygame
+# for pygame help go:  https://www.pygame.org/docs/ref/pygame.html
+from constants import *
 
 def main():
-	print("Starting Asteroids!")
+	
+	pygame.init()
+	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	looping = True
+	while looping == True:
+	
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				return
+	
+		screen.fill("black")
+		pygame.display.flip()
 
 
 
