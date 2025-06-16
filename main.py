283e2cda10833f94afc80d12df4bb449c6ce62db
EmptyVortex <malcolmshire12@gmail.com>
2025-06-16 11:13:23 +0100

    add gun

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
 	
@@ -19,10 +20,12 @@ def main():
 	updatable = pygame.sprite.Group()
 	drawable = pygame.sprite.Group()
 	asteroids = pygame.sprite.Group()
+	shots = pygame.sprite.Group()
 
 	Player.containers = (updatable, drawable)
 	Asteroid.containers = (asteroids, updatable, drawable)
 	AsteroidField.containers = (updatable)
+	Shot.containers = (shots, updatable, drawable)
 
 	player = Player(SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2)
 	field = AsteroidField()
@@ -40,6 +43,11 @@ def main():
 		for object in drawable:
 			object.draw(screen)
 		pygame.display.flip()
+		for asteroid in asteroids:
+			if asteroid.collision(player) == True:
+				print("game over")
+				return
+
 	
 		dt = clock.tick(60)/1000
 		print(dt)
--- player.py
+++ player.py
@@ -1,12 +1,14 @@
 import pygame
 from constants import *
 from circleshape import CircleShape
+from shot import Shot
 
 class Player(CircleShape):
 
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
-        self.rotation = 0   
+        self.rotation = 0
+        self.shoot_cooldown = 0
 
 
     def triangle(self):
@@ -41,3 +43,12 @@ class Player(CircleShape):
             self.move("w", dt)
         if keys[pygame.K_s]:
             self.move("s", dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+        self.shoot_cooldown -= dt
+
+    def shoot(self):
+        if self.shoot_cooldown <= 0:
+            shot = Shot(self.position.x, self.position.y)
+            shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.shoot_cooldown = 0.3
